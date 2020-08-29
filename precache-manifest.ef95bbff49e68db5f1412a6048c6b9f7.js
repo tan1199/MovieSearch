1e44816282a78b9c88e8dee2afd79713c2c2e1ce
tanmay saini <tanmay11saini11@gmail.com>
2020-08-29 23:03:56 +0530

    Updates

---
+++ precache-manifest.ef95bbff49e68db5f1412a6048c6b9f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533e0efdcb6cdd5495eeeb5a2d18e28e",
+    "url": "/MovieSearch/index.html"
+  },
+  {
+    "revision": "2c5cc628bd76b4466fc8",
+    "url": "/MovieSearch/static/css/main.84b94b48.chunk.css"
+  },
+  {
+    "revision": "1750859c486f4454c93d",
+    "url": "/MovieSearch/static/js/2.91b30a9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieSearch/static/js/2.91b30a9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5cc628bd76b4466fc8",
+    "url": "/MovieSearch/static/js/main.97a35356.chunk.js"
+  },
+  {
+    "revision": "0138f9b252cd43cea2b3",
+    "url": "/MovieSearch/static/js/runtime-main.56278f62.js"
+  },
+  {
+    "revision": "d41982d3e2012aafbf29cba168a5d8b8",
+    "url": "/MovieSearch/static/media/default_image.d41982d3.jpg"
+  },
+  {
+    "revision": "d401446cd2b0d577223af1c6d70fd906",
+    "url": "/MovieSearch/static/media/movie.d401446c.svg"
+  },
+  {
+    "revision": "a9b6febea277704c2acdbdf2a3e82ee2",
+    "url": "/MovieSearch/static/media/spinner.a9b6febe.gif"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/MovieSearch/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
